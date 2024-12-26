9ea4bfdf6d846f6970a2992539cc351250c56da8
GJY <jy.small.space@outlook.com>
2024-12-27 02:12:30 +0800

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
+}
